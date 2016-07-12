8c59e25ac96982b6426a20c4bae42bbfd1cad572
Jean Joeris <joeris.peter@gmail.com>
2016-07-12 08:45:37 -0600

    Added sentence decoder, streamer decoder and file I/O

--- paramorse-test.rb
+++ paramorse-test.rb
@@ -6,4 +6,132 @@ class ParaTest < Minitest::Test
   def test_can_make_new_queue
     assert ParaMorse::Queue.new
   end
+
+  def test_can_push_to_queue
+    q = ParaMorse::Queue.new
+    # puts q.methods - Object.new.methods
+    q.push("1")
+    assert_equal ["1"], q.queue
+  end
+
+  def test_encode_letter
+    le = ParaMorse::LetterEncoder.new
+
+    assert_equal "1", le.encode("e")
+    assert_equal "111", le.encode("t")
+  end
+
+  def test_decode_letter
+    ld = ParaMorse::LetterDecoder.new
+
+    assert_equal "q", ld.decode("1110111010111")
+    assert_equal "r", ld.decode("1011101")
+  end
+
+  def test_encode_multiple_chars
+    e = ParaMorse::Encoder.new
+    assert_equal "10111000111010101", e.encode("ab")
+    assert_equal "11101010100010111000111010100011101110100010001011101", e.encode("badger")
+  end
+
+  def test_decode_multiple_chars
+    d = ParaMorse::Decoder.new
+
+    assert_equal "ee", d.decode("10001")
+    assert_equal "foo", d.decode("1010111010001110111011100011101110111")
+    assert_equal "badger", d.decode("11101010100010111000111010100011101110100010001011101")
+  end
+
+  def test_encode_words
+    e = ParaMorse::Encoder.new
+    badger_encoded = "11101010100010111000111010100011101110100010001011101"
+    expected_string = badger_encoded + "0000000" + badger_encoded
+    star_wars_encoded = "10101000111000101110001011101" + "0000000" + "10111011100010111000101110100010101"
+    assert_equal "10001000000010001", e.encode("ee ee")
+    assert_equal expected_string, e.encode("badger badger")
+    assert_equal star_wars_encoded, e.encode("star wars")
+  end
+
+  def test_decode_words
+    d = ParaMorse::Decoder.new
+    star_wars_encoded = "10101000111000101110001011101" + "0000000" + "10111011100010111000101110100010101"
+    assert_equal "ee ee", d.decode("10001000000010001")
+    assert_equal "badger badger", d.decode("11101010100010111000111010100011101110100010001011101000000011101010100010111000111010100011101110100010001011101")
+    assert_equal "star wars", d.decode(star_wars_encoded)
+  end
+
+  def test_stream_decoder_exists
+    s = ParaMorse::StreamDecoder.new
+
+    assert s
+  end
+
+  def test_stream_decoder_1_char
+    # skip
+    s = ParaMorse::StreamDecoder.new
+    s.receive("1")
+
+    assert_equal "e", s.decode
+  end
+
+  def test_stream_decode_resets_after_each_decode
+    s = ParaMorse::StreamDecoder.new
+    s.receive("1")
+    assert_equal "e", s.decode
+    s.receive("1")
+    s.receive("1")
+    s.receive("1")
+    assert_equal "t", s.decode
+  end
+
+  def test_stream_decode_a_word
+    s = ParaMorse::StreamDecoder.new
+    badger_encoded = "11101010100010111000111010100011101110100010001011101"
+    badger_encoded.chars.each do |char|
+      s.receive(char)
+    end
+
+    assert_equal "badger", s.decode
+  end
+
+  def test_stream_decode_multiple_words
+    s = ParaMorse::StreamDecoder.new
+    star_wars_encoded = "10101000111000101110001011101" + "0000000" + "10111011100010111000101110100010101"
+    star_wars_encoded.chars.each do |char|
+      s.receive(char)
+    end
+
+    assert_equal "star wars", s.decode
+  end
+
+  def test_deletes_trailing_zeros
+    # skip
+    s = ParaMorse::StreamDecoder.new
+    s.receive("1")
+    s.receive("0")
+    assert_equal "e", s.decode
+    s.receive("1")
+    s.receive("1")
+    s.receive("1")
+    20.times do
+      s.receive("0")
+    end
+    assert_equal "t", s.decode
+  end
+
+  def test_file_encoder
+    fe = ParaMorse::FileEncoder.new
+    e = ParaMorse::Encoder.new
+    example_string = "the quick brown fox jumped over the lazy dog"
+    assert_equal e.encode(example_string).length, fe.encode("example.txt", "encoded_example.txt")
+  end
+
+  def test_file_decoder
+    # skip
+    fd = ParaMorse::FileDecoder.new
+    d = ParaMorse::Decoder.new
+    example_string = "11100010101010001000000011101110101110001010111000101000111010111010001110101110000000111010101000101110100011101110111000101110111000111010000000101011101000111011101110001110101011100000001011101110111000101011100011101110001011101110100010001110101000000011101110111000101010111000100010111010000000111000101010100010000000101110101000101110001110111010100011101011101110000000111010100011101110111000111011101"
+    assert_equal d.decode(example_string).length, fd.decode("encoded_example_copy.txt", "decoded_example.txt")
+  end
+
 end
--- paramorse.rb
+++ paramorse.rb
@@ -1,5 +1,5 @@
 #Iterations 0,1,2,3
-
+require 'pry'
 module ParaMorse
   Morse = {
     "a" => "10111",
@@ -105,7 +105,15 @@ module ParaMorse
   end
 
   class Encoder
-    def encode(word)
+    def encode(sentence)
+      words = sentence.split(" ")
+      encoded_words = words.map do |word|
+        encode_word(word)
+      end
+      encoded_words.join("0000000")
+    end
+
+    def encode_word(word)
       e = LetterEncoder.new
       letters = word.downcase.split("")
       letters.map{ |letter| e.encode(letter)}.join("000")
@@ -113,10 +121,70 @@ module ParaMorse
   end
 
   class Decoder
-    def decode(binary_word)
+    def decode(sequence)
+      morse_words = sequence.split("0000000")
+      decode_words = morse_words.map do |word|
+        decode_word(word)
+      end
+      decode_words.join(" ")
+    end
+
+    def decode_word(binary_word)
       d = LetterDecoder.new
       letters = binary_word.split("000")
       letters.map{ |letter| d.decode(letter)}.join
     end
   end
+
+  class StreamDecoder
+    def initialize
+      @decoder = Decoder.new
+      @queue = ParaMorse::Queue.new
+    end
+
+    def receive(bit)
+        bit = "0" if bit.length >1
+
+      @queue.push(bit)
+    end
+
+    def decode
+      sequence = @queue.queue.join
+      @queue.queue = []
+      sequence = delete_trailing_zeroes(sequence)
+      @decoder.decode(sequence)
+    end
+
+    def delete_trailing_zeroes(sequence)
+      zero_deleted_sequence = sequence
+      while sequence.end_with?("0")
+        zero_deleted_sequence.chop!
+      end
+      zero_deleted_sequence
+    end
+  end
+
+  class FileEncoder
+    def initialize
+      @encoder = Encoder.new
+    end
+    def encode(input_file_path, encoded_file_path)
+      input_file = File.read(input_file_path)
+      input_file.gsub!("\n", " ")
+      encoded_file = File.open(encoded_file_path, "w")
+      encoded_file.write(@encoder.encode(input_file))
+    end
+  end
+
+  class FileDecoder
+    def initialize
+      @decoder = Decoder.new
+    end
+    def decode(encoded_file_path, decoded_file_path)
+      input_file = File.read(encoded_file_path)
+      encoded_file = File.open(decoded_file_path, "w")
+      encoded_file.write(@decoder.decode(input_file))
+    end
+  end
+
 end
